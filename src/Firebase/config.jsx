2922c68f96b682e2c6619fa1695f7f5acbc42459
ebonwulf <ebonwolf00@gmail.com>
2022-10-17 15:12:54 +0100

    firebase-tool installed and initialised

---
+++ src/Firebase/config.jsx
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD8O53-DfmuQteC9feFK1KfvGDcrj1O_1s',
+  authDomain: 'thedojo-f8a62.firebaseapp.com',
+  projectId: 'thedojo-f8a62',
+  storageBucket: 'thedojo-f8a62.appspot.com',
+  messagingSenderId: '625935122115',
+  appId: '1:625935122115:web:dafca73c8e0323bbb46c76',
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//time stamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
