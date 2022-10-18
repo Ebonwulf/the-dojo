b9bd05f8e4894322124b33f389e098b5a8902d14
ebonwulf <ebonwolf00@gmail.com>
2022-10-18 09:12:52 +0100

    updated useSignup hook to create a user document that stores the displayName, thumbnail and online status for use in displaying all users in a panel on the screen

--- src/hooks/useSignup.jsx
+++ src/hooks/useSignup.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react';
-import { projectAuth, projectStorage } from '../Firebase/config';
+import {
+  projectAuth,
+  projectStorage,
+  projectFirestore,
+} from '../Firebase/config';
 import { useAuthContext } from './useAuthContext';
 
 export const useSignup = () => {
@@ -30,6 +34,13 @@ export const useSignup = () => {
 
       // add display name to user
       await res.user.updateProfile({ displayName, photoURL: imgUrl });
+
+      // create a user document
+      await projectFirestore.collection('users').doc(res.user.uid).set({
+        online: true,
+        displayName,
+        photoURL: imgUrl,
+      });
 
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: res.user });
