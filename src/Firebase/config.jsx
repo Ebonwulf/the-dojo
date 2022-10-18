de5e93870ee040cfe1f89ac0f2354bebbc5b3385
ebonwulf <ebonwolf00@gmail.com>
2022-10-18 08:28:39 +0100

    config.jsx updated to import firebase storage and set up a variable for it and initialize the service

--- src/Firebase/config.jsx
+++ src/Firebase/config.jsx
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyD8O53-DfmuQteC9feFK1KfvGDcrj1O_1s',
@@ -17,8 +18,9 @@ firebase.initializeApp(firebaseConfig);
 //init services
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 //time stamp
 const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, projectStorage, timestamp };
