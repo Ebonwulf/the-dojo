9c7d0b3ce79c6f19f87f583d5015100756244cc3
ebonwulf <ebonwolf00@gmail.com>
2022-10-18 09:27:31 +0100

    useLogout hook updated to change the online status of the user before the logout is completed. Hook now implemented in the navbar component for the log out button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
+import { useLogout } from '../../hooks/useLogout';
 import './Navbar.scss';
 import Temple from '../../assets/temple.svg';
 
 const Navbar = () => {
+  const { logout, isPending } = useLogout();
   return (
     <nav className='navbar'>
       <ul>
@@ -17,7 +19,16 @@ const Navbar = () => {
           <Link to='/signup'>Signup</Link>
         </li>
         <li>
-          <button className='btn'>Logout</button>
+          {!isPending && (
+            <button className='btn' onClick={logout}>
+              Logout
+            </button>
+          )}
+          {isPending && (
+            <button className='btn' disabled>
+              Logging out...
+            </button>
+          )}
         </li>
       </ul>
     </nav>
