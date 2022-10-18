0731a4bb51d9689664f092fdaf4c88ffd836fcba
ebonwulf <ebonwolf00@gmail.com>
2022-10-18 11:51:21 +0100

    create page form set up and completed to have the name, details and date. Will add catagory and assigned users at a later date. handleSubmit function created to handle the onSubmit on the form element

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,7 +1,59 @@
+import { useState } from 'react';
 import './Create.scss';
 
 const Create = () => {
-  return <div>Create</div>;
+  const [name, setName] = useState('');
+  const [details, setDetails] = useState('');
+  const [dueDate, setDueDate] = useState('');
+  const [catagory, setCatagory] = useState('');
+  const [assignedUsers, setAssignedUsers] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, details, dueDate);
+  };
+
+  return (
+    <div className='create-form'>
+      <h2 className='page-title'>Create a new project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project name:</span>
+          <input
+            required
+            type='text'
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Project details:</span>
+          <textarea
+            required
+            type='text'
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          ></textarea>
+        </label>
+        <label>
+          <span>Due date:</span>
+          <input
+            required
+            type='date'
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
+        <label>
+          <span>Project catagory:</span>
+        </label>
+        <label>
+          <span>Assign to:</span>
+        </label>
+        <button className='btn'>Add project</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
