d32fc77cf3d777634042a07bd9dd6b8b0f27e8bf
ebonwulf <ebonwolf00@gmail.com>
2022-10-19 09:24:27 +0100

    projectFilter component created and implemented in the dashboard component. function written to filter the documents and a switch statement used to determine if a project falls into the filter selected

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,14 +1,56 @@
 import ProjectList from '../../components/projectList/ProjectList';
 import { useCollection } from '../../hooks/useCollection';
+import { useState } from 'react';
+import ProjectFilter from './ProjectFilter';
+import { useAuthContext } from '../../hooks/useAuthContext';
 import './Dashboard.scss';
 
 const Dashboard = () => {
+  const [currentFilter, setCurrentFilter] = useState('all');
   const { documents, error } = useCollection('projects');
+  const { user } = useAuthContext();
+
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter);
+  };
+
+  // using a ternary so that it doesn't throw an error because it may take a few seconds to load the documents to the page meaning that documents would be null and give an error.
+  const projects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case 'all':
+            return true;
+          case 'mine':
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case 'development':
+          case 'design':
+          case 'sales':
+          case 'marketing':
+            console.log(document.catagory, currentFilter);
+            return document.catagory === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
+
   return (
     <div>
       <h2 className='page-title'>Dashboard</h2>
       {error && <p className='error'>{error}</p>}
-      {documents && <ProjectList projects={documents} />}
+      {projects && (
+        <ProjectFilter
+          currentFilter={currentFilter}
+          changeFilter={changeFilter}
+        />
+      )}
+      {projects && <ProjectList projects={projects} />}
     </div>
   );
 };
