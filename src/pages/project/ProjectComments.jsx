1007bef57862d668164653184e2d4ff4da4b188a
ebonwulf <ebonwolf00@gmail.com>
2022-10-19 09:57:30 +0100

    date-fns installed for use ing the projectComments component so that each comment made gives a time stamp like in fb eg. 2 mins ago or about a minute ago

--- src/pages/project/ProjectComments.jsx
+++ src/pages/project/ProjectComments.jsx
@@ -3,6 +3,7 @@ import Avatar from '../../components/avatar/Avatar';
 import { timestamp } from '../../Firebase/config';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useFirestore } from '../../hooks/useFirestore';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 const ProjectComments = ({ project }) => {
   const [newComment, setNewComment] = useState('');
@@ -11,6 +12,7 @@ const ProjectComments = ({ project }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     const commentToAdd = {
       displayName: user.displayName,
       photoURL: user.photoURL,
@@ -38,7 +40,12 @@ const ProjectComments = ({ project }) => {
                 <p>{comment.displayName}</p>
               </div>
               <div className='comment-date'>
-                <p>date here</p>
+                <p>
+                  {formatDistanceToNow(comment.createdAt.toDate(), {
+                    addSuffix: true,
+                  })}{' '}
+                  {/*using addSuffix and setting it to true add the word ago to the end of the date format eg. 2 minutes ago */}
+                </p>
               </div>
               <div className='comment-content'>
                 <p>{comment.content}</p>
@@ -52,8 +59,7 @@ const ProjectComments = ({ project }) => {
           <textarea
             required
             onChange={(e) => setNewComment(e.target.value)}
-            value={newComment}
-          ></textarea>
+            value={newComment}></textarea>
         </label>
         <button className='btn'>Add comment</button>
       </form>
